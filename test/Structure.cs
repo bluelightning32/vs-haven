8047cbcfe0220b6064cc59a6db8a0245d54cb39d
Kyle Stemen <bluelightning32@gmail.com>
2025-09-07 20:45:02 -0700

    Add another unit test

--- test/Structure.cs
+++ test/Structure.cs
@@ -27,4 +27,20 @@ public class Structure {
         stone.Select(Framework.Server.AssetManager, rand).ToList();
     Assert.IsGreaterThanOrEqualTo(1, schematics.Count);
   }
+
+  [TestMethod]
+  public void SelectCountRoundDown() {
+    Real.Structure stone = Load("stone");
+    stone.Count.dist = EnumDistribution.UNIFORM;
+    stone.Count.avg = 1.0f;
+    stone.Count.var = 0.5f;
+    NormalRandom rand = new(0);
+    int count = 0;
+    int attempts = 10;
+    for (int i = 0; i < attempts; ++i) {
+      count += stone.Select(Framework.Server.AssetManager, rand).Count();
+    }
+    Assert.IsLessThanOrEqualTo(0.7 * attempts, count);
+    Assert.IsGreaterThanOrEqualTo(0.3 * attempts, count);
+  }
 }
