5041b2ef445fb2f182a7da41d8fe04dc8a003239
Kyle Stemen <bluelightning32@gmail.com>
2025-09-13 14:24:21 -0700

    Rename GetAdjustedRadius

--- src/ResourceZonePlanner.cs
+++ src/ResourceZonePlanner.cs
@@ -4,17 +4,16 @@ namespace Haven;
 
 public class ResourceZonePlanner {
   /// <summary>
-  /// Find the adjusted radius of the circle such that moving the rectangle out
-  /// that far in the given angle results in the given point on the rectangle
-  /// touching the circle.
+  /// Finds the distance between a point (x,y) inside a circle and the boundary
+  /// of the circle, when the point is moved at the given angle.
   /// </summary>
   /// <param name="angle">angle to move the rectangle</param>
   /// <param name="radius">radius of the circle</param>
   /// <param name="x">x coordinate of a point on the rectangle</param>
   /// <param name="y">y coordinate of a point on the rectangle</param>
   /// <returns></returns>
-  public static double GetAdjustedRadius(double angle, double radius, double x,
-                                         double y) {
+  public static double GetPointToCircleDist(double angle, double radius,
+                                           double x, double y) {
     // This is based on
     // https://mathworld.wolfram.com/Circle-LineIntersection.html.
     (double sin, double cos) = Math.SinCos(angle);
--- test/ResourceZonePlanner.cs
+++ test/ResourceZonePlanner.cs
@@ -6,10 +6,10 @@ namespace Haven.Test;
 
 [PrefixTestClass]
 public class ResourceZonePlanner {
-  public void TestGetAdjustedRadius(double angle, double radius, double x,
-                                    double y) {
+  public void TestGetPointToCircleDist(double angle, double radius, double x,
+                                      double y) {
     double rAdjusted =
-        Real.ResourceZonePlanner.GetAdjustedRadius(angle, radius, x, y);
+        Real.ResourceZonePlanner.GetPointToCircleDist(angle, radius, x, y);
     double xShifted = x + rAdjusted * Math.Cos(angle);
     double yShifted = y + rAdjusted * Math.Sin(angle);
     Assert.AreEqual(
@@ -17,32 +17,32 @@ public class ResourceZonePlanner {
   }
 
   [TestMethod]
-  public void GetAdjustedRadiusHorizontal() {
-    TestGetAdjustedRadius(0, 20, 3, 3.5);
+  public void GetPointToCircleDistHorizontal() {
+    TestGetPointToCircleDist(0, 20, 3, 3.5);
   }
 
   [TestMethod]
-  public void GetAdjustedRadiusVertical() {
-    TestGetAdjustedRadius(Math.PI / 2, 20, 3, 3.5);
+  public void GetPointToCircleDistVertical() {
+    TestGetPointToCircleDist(Math.PI / 2, 20, 3, 3.5);
   }
 
   [TestMethod]
-  public void GetAdjustedRadiusQ1() {
-    TestGetAdjustedRadius(Math.PI / 3, 20, 3, 3.5);
+  public void GetPointToCircleDistQ1() {
+    TestGetPointToCircleDist(Math.PI / 3, 20, 3, 3.5);
   }
 
   [TestMethod]
-  public void GetAdjustedRadiusQ2() {
-    TestGetAdjustedRadius(2 * Math.PI / 3, 20, 3, 3.5);
+  public void GetPointToCircleDistQ2() {
+    TestGetPointToCircleDist(2 * Math.PI / 3, 20, 3, 3.5);
   }
 
   [TestMethod]
-  public void GetAdjustedRadiusQ3() {
-    TestGetAdjustedRadius(5 * Math.PI / 4, 20, 3, 3.5);
+  public void GetPointToCircleDistQ3() {
+    TestGetPointToCircleDist(5 * Math.PI / 4, 20, 3, 3.5);
   }
 
   [TestMethod]
-  public void GetAdjustedRadiusQ4() {
-    TestGetAdjustedRadius(7 * Math.PI / 4, 20, 3, 3.5);
+  public void GetPointToCircleDistQ4() {
+    TestGetPointToCircleDist(7 * Math.PI / 4, 20, 3, 3.5);
   }
 }
